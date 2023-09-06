8e1de5d4169cb1d9ec29dd8693ec367f512a2356
CristianBitca <136974309+CristianBitca@users.noreply.github.com>
2023-09-06 10:02:40 +0100

    Final BEFORE poster

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import logo from '../logo2.png';
+  import React from 'react';
+  import logo from '../logo2.png';
 
-function Header() {
-  return (
-    <header className="App-header">
-      <div className="container">
-        <img src={logo} alt="logo" />
-        <h1>Metasearch Engine For Cheap Flights</h1>
-      </div>
-    </header>
-  );
-}
+  function Header() {
+    return (
+      <header className="App-header">
+        <div className="container">
+        <a href="https://www.aerohunt.net">
+          <img src={logo} alt="logo" /></a>
+          <h1>Metasearch Engine For Cheap Flights</h1>
+        </div>
+      </header>
+    );
+  }
 
-export default Header;
+  export default Header;
